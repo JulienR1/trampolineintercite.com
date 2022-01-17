f52ae294d0100dc2b495515ba2e355d90f53f801
Julien Rousseau <julrousseau20@gmail.com>
2022-01-16 22:49:24 -0500

    Redirect to real domain in temporary production setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+
 export default function Home() {
+	useEffect(() => {
+		location.assign("https://www.trampolineintercite.com");
+	}, []);
+
 	return <div>Hello world!</div>;
 }
