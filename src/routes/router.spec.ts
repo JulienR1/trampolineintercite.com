59ccd27c1a3575b42529a688a43b9ce78fc34d20
Julien Rousseau <julrousseau20@gmail.com>
2022-01-23 01:12:25 -0500

    Added page count test

--- src/routes/router.spec.ts
+++ src/routes/router.spec.ts
@@ -1,17 +1,11 @@
 import path from "path";
-import { existsSync } from "fs";
+import { existsSync, readdirSync } from "fs";
 import { Routes } from "./routes";
 
 describe("Router", () => {
 	it("should find each registered route files", () => {
-		const filteredRoutes = new Set<string>();
-		Object.values(Routes).forEach((route) => {
-			const routeWithoutAlias = route.split("#")[0];
-			filteredRoutes.add(routeWithoutAlias);
-		});
-
 		const missingFiles: string[] = [];
-		filteredRoutes.forEach((route) => {
+		getFilteredRoutes().forEach((route) => {
 			const filepath = path.join("src/pages", `${route}.tsx`);
 			const secondFilepath = path.join("src/pages", route, "index.tsx");
 			if (!(existsSync(filepath) || existsSync(secondFilepath))) {
@@ -21,6 +15,35 @@ describe("Router", () => {
 
 		expect(missingFiles).toEqual([]);
 	});
+
+	it("should find the correct amount of pages", () => {
+		const dirPath = path.join("src", "pages");
+		const fileCount = readFileCountInDir(dirPath);
+		expect(fileCount).toBe(getFilteredRoutes().size);
+	});
+
+	const getFilteredRoutes = (): Set<string> => {
+		const filteredRoutes = new Set<string>();
+		Object.values(Routes).forEach((route) => {
+			if (!route.startsWith("http")) {
+				const routeWithoutAlias = route.split("#")[0];
+				filteredRoutes.add(routeWithoutAlias);
+			}
+		});
+		return filteredRoutes;
+	};
+
+	const readFileCountInDir = (dirPath: string): number => {
+		return readdirSync(dirPath, { withFileTypes: true }).reduce((total, currentDirElement) => {
+			if (currentDirElement.isDirectory()) {
+				return total + readFileCountInDir(path.join(dirPath, currentDirElement.name));
+			} else if (currentDirElement.name.endsWith(".tsx") && currentDirElement.name !== "_app.tsx") {
+				return total + 1;
+			} else {
+				return total;
+			}
+		}, 0);
+	};
 });
 
 export {};
