67fe53a74dc467cf7719369c92e196987d09795c
Julien Rousseau <julrousseau20@gmail.com>
2022-01-23 11:55:43 -0500

    Fixed routes typing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,36 +1,39 @@
 enum BaseRoutes {
-	HOME = "index",
-	ABOUT = "a-propos",
-	CONTACT = "contact",
-	SCHEDULE = "horaire",
+	HOME = "/",
+	ABOUT = "/a-propos",
+	CONTACT = "/contact",
+	SCHEDULE = "/horaire",
 }
 
 enum ActivityRoutes {
-	ACTIVITIES = "activites",
-	ACTIVITY_DETAILS = "activites/details",
-	ACTIVITY_RECREATIVE = "activites/details#recreatif",
-	ACTIVITY_COMPETITIVE = "activites/details#competitif",
-	ACTIVITY_PARTIES = "activites/details#fetes",
+	ACTIVITIES = "/activites",
+	ACTIVITY_DETAILS = "/activites/details",
+	ACTIVITY_RECREATIVE = "/activites/details#recreatif",
+	ACTIVITY_COMPETITIVE = "/activites/details#competitif",
+	ACTIVITY_PARTIES = "/activites/details#fetes",
 }
 
 enum TeamRoutes {
-	TEAMS = "equipes",
+	TEAMS = "/equipes",
 }
 
 enum CommunicationRoutes {
-	NEWS = "actualites",
-	MESSAGES = "annonces",
-	REGISTRATION_PROCESS = "comment-sinscrire",
-	RULES = "reglements",
+	NEWS = "/actualites",
+	MESSAGES = "/annonces",
+	REGISTRATION_PROCESS = "/comment-sinscrire",
+	RULES = "/reglements",
 }
 
 enum StaticRoutes {
 	SPORT_N_ROLL = "https://app.sportnroll.com/#/registration/21ef84af-f7c1-4f3e-a182-729a8ca963f8",
 }
 
-export const Routes = { ...BaseRoutes, ...ActivityRoutes, ...TeamRoutes, ...CommunicationRoutes, ...StaticRoutes };
-export type Route = typeof BaseRoutes &
-	typeof ActivityRoutes &
-	typeof TeamRoutes &
-	typeof CommunicationRoutes &
-	typeof StaticRoutes;
+export const Routes = {
+	...BaseRoutes,
+	...ActivityRoutes,
+	...TeamRoutes,
+	...CommunicationRoutes,
+	...StaticRoutes,
+};
+
+export type Route = BaseRoutes | ActivityRoutes | TeamRoutes | CommunicationRoutes | StaticRoutes;
