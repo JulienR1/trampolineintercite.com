18f5578326aac35ec058bd599cad0c9890abae54
Julien Rousseau <julrousseau20@gmail.com>
2022-12-06 23:56:31 -0500

    Added empty pages + basic layout

--- src/routes/route-labels.ts
+++ src/routes/route-labels.ts
@@ -1,11 +1,21 @@
 import { RouteDetails, router } from "./router";
+import type { Route } from "./routes";
 
-type RouteLabels = { [key: string]: string };
+type RouteLabels = { [key in Route]: string };
 
-const labelReducer = (labels: RouteLabels, currentRoute: RouteDetails) => ({
+const labelReducer = (
+  labels: RouteLabels,
+  currentRoute: RouteDetails,
+): RouteLabels => ({
   ...labels,
   [currentRoute.route]: currentRoute.label,
   ...(currentRoute.subroutes?.reduce(labelReducer, {} as RouteLabels) ?? {}),
 });
 
-export const routeLabels = router.reduce(labelReducer, {} as RouteLabels);
+const routeLabels = router.reduce(labelReducer, {} as RouteLabels);
+
+export const getPageName = (page: Route) => {
+  const label = routeLabels[page] ?? "";
+  const formattedLabel = label ? `| ${label}` : "";
+  return `Trampoline Intercité ${formattedLabel}`;
+};
