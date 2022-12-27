0133a94cec011ff1f2deae21b0c074c3926b1f66
Julien Rousseau <julrousseau20@gmail.com>
2022-12-26 21:08:31 -0500

    Added deployment tool for server

---
+++ server/deploy/README.md
@@ -0,0 +1,15 @@
+# Server deployment process
+
+Make `.env` file. Add
+
+```
+RENDER_API_KEY=
+SERVICE_ID=
+```
+
+Execute these commands to trigger a deployment.
+
+```bash
+npm i
+npm start
+```
---
+++ server/deploy/src/index.js
@@ -0,0 +1,91 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const renderApiKey = process.env.RENDER_API_KEY;
+const serviceId = process.env.SERVICE_ID;
+
+if (!renderApiKey) {
+  throw new Error("No api key has been provided.");
+}
+
+if (!serviceId) {
+  throw new Error("No service id has been provided.");
+}
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+let deploymentId = undefined;
+
+try {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: "Bearer " + renderApiKey,
+    },
+    body: JSON.stringify({ clearCache: "do_not_clear" }),
+  };
+
+  const response = await fetch(
+    `https://api.render.com/v1/services/${serviceId}/deploys`,
+    options
+  );
+
+  const data = await response.json();
+  deploymentId = data.id;
+  console.log(`Deploying commit '${data.commit.message}'`);
+  console.log(
+    `Monitor the progress here: https://dashboard.render.com/web/${serviceId}/deploys/${deploymentId}`
+  );
+} catch (ex) {
+  console.error("Could not begin deployment on Render.");
+  console.error(ex);
+  throw ex;
+}
+
+if (deploymentId) {
+  await sleep(5000);
+
+  let deploymentIsFinished = false;
+  let currentStatus = "";
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      authorization: "Bearer " + renderApiKey,
+    },
+  };
+
+  try {
+    while (!deploymentIsFinished) {
+      const response = await fetch(
+        `https://api.render.com/v1/services/${serviceId}/deploys/${deploymentId}`,
+        options
+      );
+
+      const data = await response.json();
+      deploymentIsFinished = data.finishedAt !== null;
+      currentStatus = data.status;
+      console.log(`Deployment in progress. Status: ${currentStatus}`);
+
+      if (deploymentIsFinished) {
+        console.log("Deployment complete!");
+        console.log("Final status: " + currentStatus);
+      } else {
+        await sleep(10000);
+      }
+    }
+  } catch (ex) {
+    console.error("Could not fetch the deployment's status");
+    console.error(ex);
+    throw ex;
+  }
+
+  if (currentStatus !== "live") {
+    throw new Error("Deployment did not complete correctly. Failing.");
+  }
+} else {
+  throw new Error("Could not start the build correctly.");
+}
