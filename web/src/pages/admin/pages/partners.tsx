9261f9332696888608df3530fd4df056946a4a8a
Julien Rousseau <julrousseau20@gmail.com>
2022-12-28 23:31:30 -0500

    First pass at partner creation form (wip)

--- web/src/pages/admin/pages/partners.tsx
+++ web/src/pages/admin/pages/partners.tsx
@@ -1,7 +1,24 @@
+import { Center, Group, Paper, TextInput, Title } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 import { adminRoute } from "../__root";
 
 export const Partners = () => {
-  return <>partners</>;
+  return (
+    <Center style={{ transform: "translateY(30px)" }}>
+      <Paper px="lg" py="md" shadow="lg">
+        <form>
+          <Title size={"h3"}>Ajouter un partenaire</Title>
+
+          <TextInput label="Nom" withAsterisk />
+          <TextInput label="Description" />
+          <Group>
+            <DatePicker label="Date de début" withAsterisk />
+            <DatePicker label="Date de fin" />
+          </Group>
+        </form>
+      </Paper>
+    </Center>
+  );
 };
 
 export const partnersRoute = adminRoute.createRoute({
