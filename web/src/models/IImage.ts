3922e0ed7e578d6a21d2d49a667c246e8797ddc3
Julien Rousseau <julrousseau20@gmail.com>
2023-01-04 19:27:20 -0500

    Removed unused models

--- web/src/models/IImage.ts
+++
@@ -1,6 +0,0 @@
-export interface IImage {
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
-}
--- web/src/models/IPartner.ts
+++
@@ -1,7 +0,0 @@
-import type { IImage } from "./IImage";
-
-export interface IPartner {
-  label: string;
-  websiteUrl: string;
-  img: IImage;
-}
