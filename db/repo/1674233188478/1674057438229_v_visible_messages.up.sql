a756a3b20fc102a93990e483e9e146b08024ec0b
Julien Rousseau <julrousseau20@gmail.com>
2023-04-17 16:41:14 -0400

    added messages to fe

--- db/repo/1674233188478/1674057438229_v_visible_messages.up.sql
+++ db/repo/1674233188478/1674057438229_v_visible_messages.up.sql
@@ -2,4 +2,4 @@ CREATE OR REPLACE VIEW active_messages AS
 SELECT title, content, `start_date`
 FROM messages
 WHERE `start_date` <= NOW() and end_date >= NOW() and `visible` = TRUE
-ORDER BY `start_date`;
+ORDER BY `start_date` DESC;
--- web/src/ui/utils/formatter.ts
+++ web/src/ui/utils/formatter.ts
@@ -1,2 +1,15 @@
 export const formatPhone = (phone: string, joinCharacter: string) =>
   [phone.slice(0, 3), phone.slice(3, 6), phone.slice(6)].join(joinCharacter);
+
+export const formatDate = (date: Date): string => {
+  if (date.getHours() === 0 && date.getMinutes() === 0) {
+    return new Intl.DateTimeFormat("fr-CA", { dateStyle: "full" }).format(date);
+  }
+
+  const formattedDate = new Intl.DateTimeFormat("fr-CA", {
+    hour12: false,
+    dateStyle: "full",
+    timeStyle: "short",
+  }).format(date);
+  return formattedDate.replace(" h ", "h");
+};
