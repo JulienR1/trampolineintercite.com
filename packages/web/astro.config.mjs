5ab0b69f4d420e896a8db4dfa25e907bb8f4bb6a
Julien Rousseau <julrousseau20@gmail.com>
2025-01-20 18:31:08 -0500

    init web w/ tailwind

---
+++ packages/web/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://trampolineintercite.com",
+  integrations: [sitemap(), tailwind()],
+});
