e23cfdb21b6872966960bb98d434d396c2742405
Julien Rousseau <julrousseau20@gmail.com>
2025-01-23 14:44:26 -0500

    footer mobile styles

--- packages/web/tailwind.config.mjs
+++ packages/web/tailwind.config.mjs
@@ -2,6 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: { sans: ["Lato", "sans-serif"] },
     extend: {
       colors: {
         primary: "#f71f44",
