ca7443f86a5fb8ff3b413bf4513ccdc16cd97edc
julienrousseau <julrousseau20@gmail.com>
2025-02-02 17:08:40 -0500

    added test templates

---
+++ packages/web/src/lib/maths.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test, } from "vitest";
+
+describe("maths", function() {
+    describe("gcd", function() {
+        test("should do something", function() {
+            expect(1).to.be.equal(1)
+        })
+    })
+})
---
+++ packages/web/src/lib/maths.ts
@@ -0,0 +1,13 @@
+/***
+* Greatest Common Divisor
+*/
+export function gcd(n: number): number {
+    return 0
+}
+
+/**
+* Lowest Common Multiple
+*/
+export function lcm(n: number): number {
+    return 0
+}
