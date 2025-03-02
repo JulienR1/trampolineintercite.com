64b0f5e80f4bd3060b246834875cc39840a2fb81
julienrousseau <julrousseau20@gmail.com>
2025-03-01 23:32:15 -0500

    added exception dates to calendar

--- packages/common/src/models/activities.ts
+++ packages/common/src/models/activities.ts
@@ -1,16 +1,21 @@
 import { z } from "zod";
 
-export const Time = z.object({ hour: z.number(), minute: z.number() })
+export const Time = z.object({ hour: z.number(), minute: z.number() });
 
 export const Activity = z.object({
-    type: z.number(),
-    title: z.string(),
-    subtitle: z.string().optional(),
-    time: z.object({ start: Time, end: Time }),
-    lessons: z.object({ count: z.number(), first: z.date(), last: z.date() }),
-    price: z.number(),
-    color: z.string()
-})
+  type: z.number(),
+  title: z.string(),
+  subtitle: z.string().optional(),
+  time: z.object({ start: Time, end: Time }),
+  lessons: z.object({
+    count: z.number(),
+    first: z.date(),
+    last: z.date(),
+    exceptions: z.array(z.date()),
+  }),
+  price: z.number(),
+  color: z.string(),
+});
 
-export type Time = z.infer<typeof Time>
-export type Activity = z.infer<typeof Activity>
+export type Time = z.infer<typeof Time>;
+export type Activity = z.infer<typeof Activity>;
--- packages/web/src/lib/formatters.ts
+++ packages/web/src/lib/formatters.ts
@@ -1,3 +1,5 @@
 export const dateFormatter = new Intl.DateTimeFormat("fr-CA", {
     dateStyle: "long",
 });
+
+export const listFormatter = new Intl.ListFormat("fr-CA");
