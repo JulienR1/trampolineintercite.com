06f11245e212ac158bd042e9b4000b3b9d85f686
Julien Rousseau <julrousseau20@gmail.com>
2025-03-18 00:01:43 -0400

    started on article page

--- packages/common/src/models/people.ts
+++ packages/common/src/models/people.ts
@@ -4,6 +4,7 @@ export const Person = z.object({
   firstname: z.string(),
   lastname: z.string(),
   url: z.string().nullable(),
+  role: z.string().optional(),
 });
 
 export type Person = z.infer<typeof Person>;
