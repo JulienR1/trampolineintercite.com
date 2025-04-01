1fc27a8564dffd3a6278720366fd8ca2f3fd928c
Julien Rousseau <julrousseau20@gmail.com>
2025-04-01 00:53:00 -0400

    added results option (mobile) at bottom of article

--- packages/common/src/models/articles.ts
+++ packages/common/src/models/articles.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { object, z } from "zod";
 import { Image } from "./images";
 import { Person } from "./people";
 
@@ -30,8 +30,33 @@ export const Article = ArticleBase.and(
   z.object({
     images: z.array(Image),
     contents: z.array(z.string()),
-    results: z.unknown().nullable(),
+    results: z
+      .object({
+        title: z.string().optional(),
+        columns: z.array(z.string()),
+        sections: z.array(
+          z.object({
+            label: z.string(),
+            rows: z.array(
+              z.object({
+                athlete: Person,
+                positions: z.array(z.string().nullable()),
+              }),
+            ),
+          }),
+        ),
+      })
+      .optional(),
   }),
+).refine(
+  (obj) =>
+    !obj.results ||
+    obj.results.sections.every((section) =>
+      section.rows.every(
+        (row) => row.positions.length === obj.results?.columns.length,
+      ),
+    ),
+  { message: "Some row has more or less columns than planned." },
 );
 
 export type ArticlePreview = z.infer<typeof ArticlePreview>;
